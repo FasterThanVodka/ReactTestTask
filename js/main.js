ae0743a72aa1597f694e64ae60a0c02025bb4caf
m.novko <m.novko@plarium.com>
2017-12-29 12:27:16 +0200

    selection behaviour fixes

--- js/main.js
+++ js/main.js
@@ -1,150 +1,163 @@
-const hostUrl = "https://ec-test-react.herokuapp.com/";
-const hostUrlItems = "api/v1/items"; // response object: { width:number, height:number }
-const hostUrlPictures = "api/v1/pictures"; // response object: { pictures: ['static/1.jpg', 'static/2.jpg', ...] }
+const HOST_URL = "https://ec-test-react.herokuapp.com/";
+const HOST_URL_ITEMS = "api/v1/items"; // response object: { width:number, height:number }
+const HOST_URL_PICTURES = "api/v1/pictures"; // response object: { pictures: ['static/1.jpg', 'static/2.jpg', ...] }
 
-var pictureUrls = [];
-var cells = [];
+let pictureUrls = [];
+let cells = [];
 
-var selectedItem;
+let selectedItem1;
+let selectedItem2;
 
 function init() {
-    getData(onGetDataSuccess);
+	getData(onGetDataSuccess);
 }
 
 function getData(onResult) {
-    var items;
-    var pictures;
+	let items;
+	let pictures;
 
-    const requestPictures = new XMLHttpRequest();
-    const requestItems = new XMLHttpRequest();
+	const requestPictures = new XMLHttpRequest();
+	const requestItems = new XMLHttpRequest();
 
-    requestItems.open("GET", hostUrl + hostUrlItems, true);
-    requestItems.addEventListener("load", function(){
-        items = JSON.parse(requestItems.responseText);
+	requestItems.open("GET", HOST_URL + HOST_URL_ITEMS, true);
+	requestItems.addEventListener("load", function(){
+		items = JSON.parse(requestItems.responseText);
 
-        requestPictures.open("GET", hostUrl + hostUrlPictures, true);
-        requestPictures.addEventListener("load", function() {
-            pictures = JSON.parse(requestPictures.responseText);
+		requestPictures.open("GET", HOST_URL + HOST_URL_PICTURES, true);
+		requestPictures.addEventListener("load", function() {
+			pictures = JSON.parse(requestPictures.responseText);
 
-            onResult({
-                width:items.width,
-                height:items.height,
-                pictures:pictures
-            });
-        });
-        requestPictures.send(null);
-    });
-    requestItems.send(null);
+			onResult({
+				width:items.width,
+				height:items.height,
+				pictures:pictures
+			});
+		});
+		requestPictures.send(null);
+	});
+	requestItems.send(null);
 }
 
 function onGetDataSuccess(data) {
-    data.pictures.forEach(function(url) {
-        pictureUrls.push(hostUrl + url);
-    });
+	data.pictures.forEach(function(url) {
+		pictureUrls.push(HOST_URL + url);
+	});
 
-    createItems(data.width, data.height);
+	createItems(data.width, data.height);
 }
 
 function createItems(width, height) {
-    var count = 0;
-    var container = createItem("containerTable");
+	let count = 0;
+	let container = createItem("containerTable");
 
-    for (var i=0; i<height; ++i) {
-        var row = createItem("containerRow");
+	for (let i=0; i<height; ++i) {
+		let row = createItem("containerRow");
 
-        for (var j=0; j<width; ++j) {
-            var img = createNextImage(count);
+		for (let j=0; j<width; ++j) {
+			let img = createNextImage(count);
 
-            var cell = createItem("containerCell");
-            cell.id = "cell" + count++;
-            cell.imageType = function() { return img.imageType; };
-            cell.appendChild(img);
+			let cell = createItem("containerCell");
+			cell.id = "cell" + count++;
+			cell.imageType = function() { return img.imageType; };
+			cell.appendChild(img);
 
-            cells.push(cell);
+			cells.push(cell);
 
-            row.appendChild(cell);
-        }
+			row.appendChild(cell);
+		}
 
-        container.appendChild(row);
-    }
+		container.appendChild(row);
+	}
 
-    shuffleImages();
+	shuffleImages();
 
-    document.body.appendChild(container);
+	document.body.appendChild(container);
 
-    setTimeout(function() {
-        cells.forEach(function(item) {
-            unselectItem(item.getElementsByTagName("img")[0]);
-        })
-    }, 3000);
+	setTimeout(function() {
+		cells.forEach(function(item) {
+			unselectItem(getImg(item));
+		})
+	}, 3000);
 }
 
 function createItem(classType) {
-    var result = document.createElement("div");
-        result.className = classType;
-    return result;
+	let result = document.createElement("div");
+		result.className = classType;
+	return result;
 }
 
 function createNextImage(count) {
-    var nextIndex = count % pictureUrls.length;
-    var result = new Image();
-        result.imageType = nextIndex;
-        result.src = pictureUrls[nextIndex];
-        result.addEventListener("click", onItemClick);
-    return result;
+	let nextIndex = count % pictureUrls.length;
+	let result = new Image();
+		result.imageType = nextIndex;
+		result.src = pictureUrls[nextIndex];
+		result.addEventListener("click", onItemClick);
+	return result;
 }
 
 function shuffleImages() {
-    for (var i=0; i<cells.length-1; ++i) {
-        swapItems(cells[i], cells[Math.ceil(Math.random() * cells.length-1)]);
-    }
+	for (let i=0; i<cells.length-1; ++i) {
+		swapItems(cells[i], cells[Math.ceil(Math.random() * cells.length-1)]);
+	}
 }
 
 function swapItems(item1, item2) {
-    var image1 = item1.getElementsByTagName("img")[0];
-    var image2 = item2.getElementsByTagName("img")[0];
+	let image1 = getImg(item1);
+	let image2 = getImg(item2);
 
-    var tempImageSrc = image1.src;
-    image1.src = image2.src;
-    image2.src = tempImageSrc;
+	let tempImageSrc = image1.src;
+	image1.src = image2.src;
+	image2.src = tempImageSrc;
 
-    var tempImageType = image1.imageType;
-    image1.imageType = image2.imageType;
-    image2.imageType = tempImageType;
+	let tempImageType = image1.imageType;
+	image1.imageType = image2.imageType;
+	image2.imageType = tempImageType;
+}
+
+function getImg(item) {
+	return item.getElementsByTagName("img")[0];
 }
 
 function onItemClick(event) {
-    var targetItem = event.target;
+	let targetItem = event.target;
 
-    if (selectedItem) {
-        if (selectedItem.imageType == targetItem.imageType) {
-            selectedItem.parentNode.innerHTML = targetItem.parentNode.innerHTML = "<p>x</p>";
-        } else {
-            selectItem(targetItem);
-        }
+	if (selectedItem1 && selectedItem1 != targetItem)
+	{
+		selectedItem2 = targetItem;
 
-    } else {
-        selectItem(targetItem);
-    }
+		if (selectedItem.imageType == targetItem.imageType) {
+			selectedItem.parentNode.innerHTML = targetItem.parentNode.innerHTML = "<p>x</p>";            
+			selectedItem1 = null;
+			selectedItem2 = null;
+		} else {
+			unselectItem(selectedItem1);
+			unselectItem(selectedItem2);
+			selectedItem1 = null;
+			selectedItem2 = null;
+		}
+	} else {
+		selectItem(targetItem);
+		selectedItem1 = targetItem;
+	}
+}
+
+function isSelected(item) {
+	return !!item.style.opacity;
 }
 
 function selectItem(item) {
-    selectedItem = item;
+	selectedItem = item;
 
-    item.style.opacity = 1;
+	item.style.opacity = 1;
 
-    var delayId;
-        delayId = setTimeout(function() {
-        unselectItem(item);
-        clearTimeout(delayId);
-    }, 2000);
+	let delayId;
+		delayId = setTimeout(function() {
+		unselectItem(item);
+		clearTimeout(delayId);
+	}, 1200);
 }
 
 function unselectItem(item) {
-    if (item)
-    {
-        item.style.opacity = 0.0;
-    }
-
-    selectedItem = null;
+	if (item)
+		item.style.opacity = 0;
 }
